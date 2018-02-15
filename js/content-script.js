f90e2a64654456709fca2c5daa4e7ef9b934d6c2
zjf <1010543618@qq.com>
2018-02-15 12:19:34 +0800

    Fixed some bugs.

--- js/content-script.js
+++ js/content-script.js
@@ -10,25 +10,38 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
     })
     sendResponse(JSON.stringify(pre_tag));
   }
+
   if(request.cmd == 'OT'){
-    translated_page_pretreatment();
+    if (!$('#gptp_pretreatmented').length) {
+      alert('请先点击《翻译》翻译此网页');
+      return;
+    }
     injectCustomJs("_intlStrings._originalText = '中文译文'");
     $('[dh-lang=ori]').removeClass('google-src-text');
     $('[dh-lang=tran]').addClass('google-src-text');
   }
   if(request.cmd == 'TO'){
-    translated_page_pretreatment();
+    if (!$('#gptp_pretreatmented').length) {
+      alert('请先点击《翻译》翻译此网页');
+      return;
+    }
     injectCustomJs("_intlStrings._originalText = '英语原文'");
     $('[dh-lang=tran]').removeClass('google-src-text');
     $('[dh-lang=ori]').addClass('google-src-text');
   }
   if(request.cmd == 'OOT'){
-    translated_page_pretreatment();
+    if (!$('#gptp_pretreatmented').length) {
+      alert('请先点击《翻译》翻译此网页');
+      return;
+    }
     $('[dh-lang=ori]').removeClass('google-src-text');
     $('[dh-lang=tran]').removeClass('google-src-text');
   }
   if(request.cmd == 'toggle_edit'){
-    console.log($('#gptp-is-edit'));
+    if (!$('#gptp_pretreatmented').length) {
+      alert('请先点击《翻译》翻译此网页');
+      return;
+    }
     if ($('#gptp-is-edit').length) {
       // 全部不可编辑
       $('*').removeAttr('contenteditable');
@@ -45,7 +58,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
       // 去除可编辑flag
       $('#gptp-is-edit').remove();
     }else{
-      translated_page_pretreatment();
       // 改变文字颜色
       $('[dh-lang=ori]').addClass('gptp-color-red');
       $('[dh-lang=tran]').addClass('gptp-color-blue');
@@ -64,7 +76,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 
 $(function(){
   goto_translated_page();
-  replace_pre_html();
+  if (location.href.startsWith('https://translate.googleusercontent.com/translate_c?')) {
+    replace_pre_html();
+    translated_page_pretreatment();
+  }
 })
 
 
@@ -78,46 +93,37 @@ function goto_translated_page(){
 
 // 替换pre标签的html（translate_c）
 function replace_pre_html(){
-  if (location.href.startsWith('https://translate.googleusercontent.com/translate_c?')) {
-    chrome.runtime.sendMessage({cmd: 'get_pre'}, function(response) {
-      console.log('replace_pre_html收到来自popup.js的回复：'+response);
-      var pre_html = JSON.parse(response);
-      if (pre_html) {
-        $('pre').each(function(index, element){
-          $(element).html(pre_html[index]);
-        })
-      }
-    });
-  }
+  chrome.runtime.sendMessage({cmd: 'get_pre'}, function(response) {
+    console.log('replace_pre_html收到来自popup.js的回复：'+response);
+    var pre_html = JSON.parse(response);
+    if (pre_html) {
+      $('pre').each(function(index, element){
+        $(element).html(pre_html[index]);
+      })
+    }
+  });
 }
 
-// 处理翻译过的界面
+// 处理翻译过的界面（translate_c）
 function translated_page_pretreatment(){
-  // 检查是否处理过
-  if ($('#gptp_pretreatmented').length) {
-    return
-  }
-  if (location.href.startsWith('https://translate.googleusercontent.com/translate_c?')) {
-    // 获取全部翻译过的节点
-    var $translateNodes = $('span.notranslate');
-    $translateNodes.each(function(){
-      var $translateNodeChilds = $(this).children();
-      var translatechildNodes = this.childNodes;
-      $translateNodeChilds.first().attr("dh-lang","ori");
-      var $spanZh = $('<span></span>');
-      // 使用$translateNodeChilds.each()纯文本节点无法转移
-      // 所以使用this.childNodes（translatechildNodes）
-      for (var i = translatechildNodes.length - 1; i > 0; i--) {
-        $spanZh.append($(translatechildNodes[1]));
-      }
-      $spanZh.attr("dh-lang","tran");
-      $(this).append($spanZh);
-    });
-    // 添加处理过的标识
-    $('body').append('<div id="gptp_pretreatmented"></div>');
-  }else{
-    alert('请先点击《翻译》翻译此网页');
-  }
+  // 获取全部翻译过的节点
+  var $translateNodes = $('span.notranslate');
+  $translateNodes.each(function(){
+    var $translateNodeChilds = $(this).children();
+    var translatechildNodes = this.childNodes;
+    $translateNodeChilds.first().attr("dh-lang","ori");
+    var $spanZh = $('<span></span>');
+    // 使用$translateNodeChilds.each()纯文本节点无法转移
+    // 所以使用this.childNodes（translatechildNodes）
+    for (var i = translatechildNodes.length - 1; i > 0; i--) {
+      $spanZh.append($(translatechildNodes[1]));
+    }
+    $spanZh.attr("dh-lang","tran");
+    $(this).append($spanZh);
+  });
+  // 添加处理过的标识
+  $('body').append('<div id="gptp_pretreatmented"></div>');
+
 }
 
 
@@ -130,10 +136,12 @@ function injectCustomJs(js)
   // 获得的地址类似：chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/js/inject.js
   // temp.src = chrome.extension.getURL(jsPath);
   temp.innerHTML = js;
-  temp.onload = function()
-  {
+  /*不是加载外部文件，直接移除就行
+  temp.onload = function(){
+    console.log(this);
     // 放在页面不好看，执行完后移除掉
     this.parentNode.removeChild(this);
-  };
+  };*/
   document.body.appendChild(temp);
+  temp.parentNode.removeChild(temp);
 }
